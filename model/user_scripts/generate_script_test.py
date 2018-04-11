1b5d224eefde90349f8a4a24ad13bcddb0fa59a8
Tsalexey <github.kalmar-1o1D>
2018-04-12 00:07:01 +0300

    fix

---
+++ model/user_scripts/generate_script_test.py
@@ -0,0 +1,44 @@
+import sys
+
+sys.path.append('../')
+
+from stats.statisticsmanager import StatisticsManager
+from utils.csvmanager import CSVManager
+from utils.parametersparser import ParametersParser
+
+def main():
+    names = [
+        "test"
+    ];
+
+    names2 = [
+        "test2"
+    ];
+
+    generate(names, "m/m/c[c0]/r[l,h]", "requests");
+    generate(names2, "m/m/c[c0]/r", "requests");
+
+def generate(names, mode, strategy):
+    parser = ParametersParser()
+
+    for name in names:
+        [parameters, config] = parser.parse_parameters(name + ".config")
+
+        stat_manager = StatisticsManager(parameters, mode, strategy)
+        generated_stat = stat_manager.generate_statistics()
+
+        csv_dto = stat_manager.transform_to_csv_dto(generated_stat, config)
+
+        csv_manager = CSVManager()
+        csv_manager.write_csv(csv_dto)
+
+        print("Finish! Gathered ", len(generated_stat), " results to storage, all result saved in ", csv_dto.filename,
+              ".csv")
+
+        csv_dto.filename = csv_dto.filename
+        csv_manager.write_csv(csv_dto)
+
+
+if __name__ == '__main__':
+	main()
+
